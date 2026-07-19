ff75c723e7398b17b75f631dc9c5239c660c7514
Bryan <bg478806@gmail.com>
2026-07-19 14:05:06 -0400

    Add directory path to sys.path in Constructor.py for module accessibility

--- Main-Code/Constructor.py
+++ Main-Code/Constructor.py
@@ -1,4 +1,13 @@
 
+import sys
+import os
+
+# This finds the 'Prometheus' directory and adds it to the search path
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+
 from Research.node import Node
 root = Node("animal", 100)
 
